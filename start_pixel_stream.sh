af71140f63465a2c410ff56e4f1307f91725c694
Ace <50378182+Fried-Squid@users.noreply.github.com>
2024-11-18 02:33:43 +0000

    Update start_pixel_stream.sh
    Changed directories

--- start_pixel_stream.sh
+++ start_pixel_stream.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-UNREAL="/home/aceh/Downloads/Unreal/Engine/Binaries/Linux"
+UNREAL="/opt/Unreal/Engine/Binaries/Linux"
 tmux new-session -d -s UNREAL_ENGINE -c $UNREAL
 tmux send-keys -t UNREAL_ENGINE "./UnrealEditor ~/Documents/Unreal\ Projects/MyProject/MyProject.uproject -prefernvidia -PixelStreamingURL=ws://127.0.0.1:8888 -EditorPixelStreamingStartOnLaunch=true -EditorPixelStreamingUseRemoteSignallingServer=true -RenderOffScreen -EditorPixelStreamingRes=1920x1080" C-m
 
-SIGNAL="│/home/aceh/PixelStreamingInfrastructure/SignallingWebServer/platform_scripts/bash"
+SIGNAL="│/opt/PixelStreamingInfrastructure/SignallingWebServer/platform_scripts/bash"
 MATCHMAKER_ADDRESS="10.0.16.15"
 MY_IP=$(ifconfig | sed -En 's/127.0.0.1//;s/.*inet (addr:)?(([0-9]*\.){3}[0-9]*).*/\2/p')
 tmux new-session -d -s SIGNALLING_SERVER -c $SIGNAL
