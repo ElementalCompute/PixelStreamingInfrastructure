62ef5e59538602267f7ba21411960df8b6558528
Ace <50378182+Fried-Squid@users.noreply.github.com>
2024-11-19 13:34:03 +0000

    Update start_pixel_stream.sh
    Changed paths and IPs for K3S deployment

--- start_pixel_stream.sh
+++ start_pixel_stream.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 UNREAL="/opt/Unreal/Engine/Binaries/Linux"
 tmux new-session -d -s UNREAL_ENGINE -c $UNREAL
-tmux send-keys -t UNREAL_ENGINE "./UnrealEditor ~/Documents/Unreal\ Projects/MyProject/MyProject.uproject -prefernvidia -PixelStreamingURL=ws://127.0.0.1:8888 -EditorPixelStreamingStartOnLaunch=true -EditorPixelStreamingUseRemoteSignallingServer=true -RenderOffScreen -EditorPixelStreamingRes=1920x1080" C-m
+tmux send-keys -t UNREAL_ENGINE "./UnrealEditor /opt/projects/MyProject/MyProject.uproject -prefernvidia -PixelStreamingURL=ws://127.0.0.1:8888 -EditorPixelStreamingStartOnLaunch=true -EditorPixelStreamingUseRemoteSignallingServer=true -RenderOffScreen -EditorPixelStreamingRes=1920x1080" C-m
 
-SIGNAL="│/opt/PixelStreamingInfrastructure/SignallingWebServer/platform_scripts/bash"
+SIGNAL="/opt/PixelStreamingInfrastructure/SignallingWebServer/platform_scripts/bash"
 MATCHMAKER_ADDRESS="10.0.16.15"
-MY_IP=$(ifconfig | sed -En 's/127.0.0.1//;s/.*inet (addr:)?(([0-9]*\.){3}[0-9]*).*/\2/p')
+MY_IP="0.0.0.0"
 tmux new-session -d -s SIGNALLING_SERVER -c $SIGNAL
 tmux send-keys -t SIGNALLING_SERVER "./Start_SignallingServer.sh --UseMatchmaker true --MatchmakerAddress $MATCHMAKER_ADDRESS --MatchmakerPort 9999 --PublicIp $MY_IP --HttpPort 80 --nosudo" C-m
